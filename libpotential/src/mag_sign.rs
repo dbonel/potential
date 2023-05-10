4c6c150a588791fdff0c5fc588679187eec54cfd
Drew Bonel <drew.rotated@gmail.com>
2023-05-10 11:53:08 -0700

    MagSign: Handle unmatched channel counts better

--- libpotential/src/mag_sign.rs
+++ libpotential/src/mag_sign.rs
@@ -21,9 +21,18 @@ pub fn decompose(in_values: &[f32], out_magnitudes: &mut [f32], out_signs: &mut 
 pub fn recompose(in_magnitudes: &[f32], in_signs: &[f32], out_values: &mut [f32]) {
     debug_assert!(out_values.len() >= in_signs.len());
     debug_assert!(out_values.len() >= in_magnitudes.len());
+    // Our output polyphony channels are limited by the polyphony count on the
+    // signs input. We can default to 0.0 for magnitudes, but signs don't have a
+    // reasonable default.
+    //
+    // We pad the magnitudes with an infinite iterator of 0.0, and rely on the
+    // zip() iterator to take as many as it needs to match the length of signs.
+    let zeros = Some(0.0).iter().cycle();
+    let in_pairs = in_magnitudes.iter().chain(zeros).zip(in_signs.iter());
+
     out_values
         .iter_mut()
-        .zip(in_magnitudes.iter().zip(in_signs.iter()))
+        .zip(in_pairs)
         .for_each(|(o, (i_mag, i_sign))| {
             *o = {
                 if i_mag.is_finite() && i_sign.is_finite() {
