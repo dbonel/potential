1ebee469e2c574414f304c72393149249119bc86
Drew Bonel <drew.rotated@gmail.com>
2023-05-09 22:53:23 -0700

    Add Rust library

---
+++ libpotential/src/mag_sign.rs
@@ -0,0 +1,36 @@
+// Decompose a slice of floats into their magnitudes (the absolute values)
+// and their signs (a value that is either -1.0 or +1.0).
+pub fn decompose(in_values: &[f32], out_magnitudes: &mut [f32], out_signs: &mut [f32]) {
+    debug_assert_eq!(in_values.len(), out_magnitudes.len());
+    debug_assert_eq!(in_values.len(), out_signs.len());
+    in_values
+        .iter()
+        .zip(out_magnitudes.iter_mut().zip(out_signs.iter_mut()))
+        .for_each(|(i, (o_mag, o_sign))| {
+            (*o_mag, *o_sign) = {
+                if i.is_finite() {
+                    (i.abs(), i.signum())
+                } else {
+                    // Map infinities and NaNs to positive 0.
+                    (0.0, 1.0)
+                }
+            };
+        });
+}
+
+pub fn recompose(in_magnitudes: &[f32], in_signs: &[f32], out_values: &mut [f32]) {
+    debug_assert_eq!(in_magnitudes.len(), out_values.len());
+    debug_assert_eq!(in_signs.len(), out_values.len());
+    out_values
+        .iter_mut()
+        .zip(in_magnitudes.iter().zip(in_signs.iter()))
+        .for_each(|(o, (i_mag, i_sign))| {
+            *o = {
+                if i_mag.is_finite() && i_sign.is_finite() {
+                    i_mag.copysign(*i_sign)
+                } else {
+                    0.0
+                }
+            };
+        })
+}
---
+++ libpotential/src/util.rs
@@ -0,0 +1,46 @@
+#[derive(Debug, Default)]
+enum InputTriggerState {
+    #[default]
+    Low,
+    High,
+}
+
+// Generates trigger events from an input port's voltage.
+#[derive(Debug, Default)]
+pub struct InputTrigger {
+    state: InputTriggerState,
+}
+
+impl InputTrigger {
+    const LOW_THRESHOLD: f32 = 0.1;
+    const HIGH_THRESHOLD: f32 = 1.0;
+
+    pub fn new() -> Self {
+        InputTrigger {
+            state: InputTriggerState::Low,
+        }
+    }
+
+    pub fn process_voltage(&mut self, value: f32) -> bool {
+        use InputTriggerState::*;
+        match self.state {
+            Low => {
+                let triggered = value >= Self::HIGH_THRESHOLD;
+                if triggered {
+                    self.state = High;
+                }
+                triggered
+            }
+            High => {
+                if value <= Self::LOW_THRESHOLD {
+                    self.state = Low;
+                }
+                false
+            }
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.state = InputTriggerState::Low;
+    }
+}
