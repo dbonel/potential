a1a25b4ad961db8a9add922eaa1b346493089008
Drew Bonel <drew.rotated@gmail.com>
2023-05-12 09:39:29 -0700

    Auto-format plugin.hpp

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -6,7 +6,7 @@
 using namespace rack;
 
 // Declare the Plugin, defined in plugin.cpp
-extern Plugin* pluginInstance;
+extern Plugin *pluginInstance;
 
 // Declare each Model, defined in each module source file
-extern Model* modelMagSign;
+extern Model *modelMagSign;
