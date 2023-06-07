fe0e15b5371714059b86b018951786f988f08f30
Drew Bonel <drew.rotated@gmail.com>
2023-06-06 19:40:01 -0700

    Detect target OS in build script before adding OS-specific C++ flags

--- libpotential/build.rs
+++ libpotential/build.rs
@@ -1,8 +1,13 @@
+use std::env;
+
 fn main() {
-    cxx_build::bridge("src/ffi.rs")
-        .cpp(true)
-        // FIXME: get this from an env variable
-        .flag("-mmacosx-version-min=10.9")
-        .flag_if_supported("-std=c++11")
-        .compile("potential");
+    let mut bridge_builder = cxx_build::bridge("src/ffi.rs");
+    bridge_builder.cpp(true).flag_if_supported("-std=c++11");
+    match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
+        "macos" => {
+            bridge_builder.flag("-mmacosx-version-min=10.9");
+        }
+        _ => {}
+    }
+    bridge_builder.compile("potential");
 }
