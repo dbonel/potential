043f1e74f40945c256be228e00b64ee50b4c4b1b
Drew Bonel <drew.rotated@gmail.com>
2024-06-24 18:20:11 -0700

    Set MACOSX_DEPLOYMENT_TARGET env var from build.rs
    This seems to eliminate a warning about the Rust staticlib being built
    for a different OSX version than the rest of the plugin.

--- libpotential/build.rs
+++ libpotential/build.rs
@@ -5,7 +5,11 @@ fn main() {
     bridge_builder.cpp(true).flag_if_supported("-std=c++11");
     match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
         "macos" => {
+            // The Rack C++ SDK uses -mmacosx-version-min=10.9 when building
+            // and linking, let's try to match it in the cxx bridge and the
+            // Rust staticlib.
             bridge_builder.flag("-mmacosx-version-min=10.9");
+            println!("cargo::rustc-env=MACOSX_DEPLOYMENT_TARGET=10.9");
         }
         _ => {}
     }
