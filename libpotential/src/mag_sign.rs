37c1a808768d533cc1cd0786400fbb82e68f796b
Drew Bonel <drew.rotated@gmail.com>
2024-06-28 18:40:50 -0700

    Rewrite MagSign using new Port methods

--- libpotential/src/mag_sign.rs
+++ libpotential/src/mag_sign.rs
@@ -23,55 +23,61 @@ impl MagSign {
 
 fn mag_sign_process(inputs: &MagSignInput, outputs: &mut MagSignOutput) {
     // Upper half processing: decomposition
-    {
-        outputs.magnitude.set_polyphony_from(&inputs.bipolar);
-        outputs.sign.set_polyphony_from(&inputs.bipolar);
-        let bipolar = inputs.bipolar.as_slice();
-        let magnitude = outputs.magnitude.as_slice_mut();
-        let sign = outputs.sign.as_slice_mut();
-        bipolar
-            .iter()
-            .zip(magnitude.iter_mut().zip(sign.iter_mut()))
-            .for_each(|(bipolar_in, (magnitude_out, sign_out))| {
-                (*magnitude_out, *sign_out) = {
-                    if !bipolar_in.is_nan() {
-                        (bipolar_in.abs(), bipolar_in.signum())
-                    } else {
-                        // Map NaNs to positive 0.
-                        (0.0, 1.0)
-                    }
-                };
-            });
+    if let Some(bipolar_voltages) = inputs.bipolar.as_slice() {
+        let n = bipolar_voltages.len();
+
+        if let Some(magnitude_voltages) = outputs.magnitude.as_slice_mut_from_polyphony_count(n) {
+            assert_eq!(n, magnitude_voltages.len());
+            magnitude_voltages
+                .iter_mut()
+                .zip(bipolar_voltages)
+                .for_each(|(m, b)| {
+                    *m = b.abs();
+                });
+        }
+
+        if let Some(sign_voltages) = outputs.sign.as_slice_mut_from_polyphony_count(n) {
+            assert_eq!(n, sign_voltages.len());
+            sign_voltages
+                .iter_mut()
+                .zip(bipolar_voltages)
+                .for_each(|(s, b)| {
+                    *s = b.signum();
+                });
+        }
+    } else {
+        outputs.magnitude.set_polyphony_count(0);
+        outputs.sign.set_polyphony_count(0);
     }
 
     // Lower half processing: recomposition
-    {
-        // We only produce as many output channels as we have input signs. We
-        // can pad the input magnitudes with 0.0, but the signs are impossible
-        // to default to either positive or negative without biasing the output.
-        outputs.bipolar.set_polyphony_from(&inputs.sign);
-        let magnitude = inputs.magnitude.as_slice();
-        let sign = inputs.sign.as_slice();
-        let bipolar = outputs.bipolar.as_slice_mut();
+    //
+    // We only produce as many output channels as we have input signs. We
+    // can pad the input magnitudes with 0.0, but the signs are impossible to
+    // default to either positive or negative without biasing the output.
+    if let Some(sign_voltages) = inputs.sign.as_slice() {
+        let n = sign_voltages.len();
 
-        // We could have fewer magnitudes than signs, so we extend the magnitudes with
-        // an infinite iterator of 0.0. This will be zipped with the signs, so the
-        // resulting zip iterator will be the length of the signs.
-        let zeros = Some(0.0).iter().cycle();
-        let mag_sign_pairs = magnitude.iter().chain(zeros).zip(sign.iter());
-        bipolar
-            .iter_mut()
-            .zip(mag_sign_pairs)
-            .for_each(|(bipolar_out, (magnitude_in, sign_in))| {
-                *bipolar_out = {
-                    let result = magnitude_in.copysign(*sign_in);
-                    if !result.is_nan() {
-                        result
-                    } else {
-                        0.0
-                    }
-                };
-            })
+        if let Some(bipolar_outputs) = outputs.bipolar.as_slice_mut_from_polyphony_count(n) {
+            assert_eq!(bipolar_outputs.len(), n);
+
+            let zero_padded_magnitudes = inputs
+                .magnitude
+                .as_slice()
+                .unwrap_or_default()
+                .iter()
+                .chain(Some(0.0).iter().cycle())
+                .take(n);
+            let mag_sign_pairs = zero_padded_magnitudes.zip(sign_voltages);
+
+            bipolar_outputs.iter_mut().zip(mag_sign_pairs).for_each(
+                |(bipolar_out, (magnitude_in, sign_in))| {
+                    *bipolar_out = magnitude_in.copysign(*sign_in);
+                },
+            );
+        }
+    } else {
+        outputs.bipolar.set_polyphony_count(0);
     }
 }
 
